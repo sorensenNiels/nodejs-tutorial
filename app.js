0032c3c01869bb28fbf06767ba8debb1807abc11
Niels P. Sorensen <niels@nodesoft.dk>
2017-07-24 23:23:33 +0200

    modified

---
+++ app.js
@@ -0,0 +1,24 @@
+/* eslint no-console: "off" */
+
+const myName = "Niels P.",
+
+    sayHello = (name) => {
+        console.log(`Hello ${name}`);
+    },
+
+    obj = {
+        "name": myName,
+        "a": () => console.log("a", this.name),
+        "b" () {
+            console.log("b", this.name);
+        },
+        "c" () {
+            console.log("c", this.name);
+        }
+    };
+
+obj.a();
+obj.b();
+obj.c();
+
+sayHello(myName);
