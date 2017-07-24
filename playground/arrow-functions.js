20db1b9c32666d23e0f37850736be9547cf5889f
Niels P. Sorensen <niels@nodesoft.dk>
2017-07-24 20:43:44 +0200

    fixes

--- playground/arrow-functions.js
+++ playground/arrow-functions.js
@@ -3,21 +3,21 @@
 const _ = require("lodash");
 
 const myName = "Niels P.",
+
     sayHello = (name) => {
         console.log(`Hello ${name}`);
-    };
-
-// Arrow function within object - watch out for the this not being set
-const obj = {
-    "name": myName,
-    "a": () => console.log("a", this.name),
-    "b" () {
-        console.log("b", this.name);
     },
-    "c" () {
-        console.log("c", this.name);
-    }
-};
+
+    obj = {
+        "name": myName,
+        "a": () => console.log("a", this.name),
+        "b" () {
+            console.log("b", this.name);
+        },
+        "c" () {
+            console.log("c", this.name);
+        }
+    };
 
 obj.a();
 obj.b();
