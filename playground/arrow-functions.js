cb772c208341e4bcd2f331240647b6a57be16ea2
Niels P. Sorensen <niels@nodesoft.dk>
2017-07-24 19:54:42 +0200

    Added eslint

--- playground/arrow-functions.js
+++ playground/arrow-functions.js
@@ -1,24 +1,28 @@
-'use strict';
+"use strict";
 
 const _ = require("lodash");
 
 const myName = "Niels P.";
 
 // Arrow function with parameter
-var sayHello = (name) => {
+let sayHello = (name) => {
     console.log(`Hello ${name}`);
 };
 
 // Arrow function within object - watch out for the this not being set
-var obj = {
-    name: myName,
-    a: () => console.log('a', this.name),
-    b: function() {
-        console.log('b', this.name)
-    },
-    c() {
-        console.log('c', this.name);
-    }
+let obj = {
+    "name": myName,
+    "a": () => console.log("a", this.name),
+    "b": function () {
+
+        console.log("b", this.name);
+    
+},
+    c () {
+
+        console.log("c", this.name);
+    
+}
 };
 
 obj.a();
